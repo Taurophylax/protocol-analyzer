57fcf4af10c10f52c74b18fba2d03c023f41e185
James Carr <jcarr8@uoregon.edu>
2023-05-03 09:26:29 -0700

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+import openai
+import os
 from config import Config
 
 app = Flask(__name__) #must be before importing routes
 app.config.from_object(Config)
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 from app import routes
 
